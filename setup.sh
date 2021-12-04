ffab82593b216730c8aafbc36b99a5649ac0c13e
9bitsdelta <vlad.moreanu@yahoo.com>
2021-12-04 21:06:20 +0200

    Bugfixing setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Basic setup for Arch Linux
-# 
+#
 # you must manually add users and their groups
 
-HOST=	$1
-REGION=	$2
-CITY=	$3
+HOST=$1
+REGION=$2
+CITY=$3
 
-ln -sf /usr/share/zoneinfo/&REGION/&CITY /etc/localtime
+ln -sf /usr/share/zoneinfo/$REGION/$CITY /etc/localtime
 hwclock --systohc
 
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
@@ -17,9 +17,9 @@ locale-gen
 echo $HOST >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1 localhost" >> /etc/hosts
-echo "127.0.1.1 "&HOST".domain "$HOST >> /etc/hosts
+echo "127.0.1.1 "$HOST".domain "$HOST >> /etc/hosts
 
-pacman -S base-devel networkmanager grub efibootmgr dosfstools os-prober mtools sudo nvim
+pacman -S base-devel networkmanager grub efibootmgr dosfstools os-prober mtools neovim
 
 systemctl enable NetworkManager
 
@@ -27,3 +27,4 @@ mkdir /boot/EFI
 mount /dev/sda1 /boot/EFI
 grub-install --target=x86_64-efi --bootloader=grub_uefi --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
+
