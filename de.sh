6c29577c869e4fbf01aa112c38c9b001ce6f13bc
Vlad M <74763064+9bitsdelta@users.noreply.github.com>
2021-12-05 01:15:17 +0200

    Update de.sh

--- de.sh
+++ de.sh
@@ -6,11 +6,11 @@ pacman -S xorg xorg-xinit archlinux-wallpaper feh picom bspwm sxhkd alacritty ro
 
 git clone https://aur.archlinux.org/yay
 cd yay
-mkpg -si
+makepg -si
 cd ../
 
 yay -S polybar
 yay -Rsnd i3-wm
 
 git clone https://github.com/9bitsdelta/dotfiles
-cp -r ./dotfiles/** ~/
+cp -r ./dotfiles ~/
